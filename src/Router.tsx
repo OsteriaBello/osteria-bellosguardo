c2602adf0a9a75ebc8edd7ed29dea3237fb5abc9
OuaailElaouad <2003waelelaouad@gmail.com>
2025-11-23 01:09:01 +0100

    version stable 0

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import App from './App';
-import AdminPage from './pages/AdminPage';
+
 
 const Router = () => {
   const path = window.location.pathname;
 
-  if (path === '/admin') {
-    return <AdminPage />;
-  }
+  
 
   return <App />;
 };
